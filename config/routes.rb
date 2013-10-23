0c40d6bbec1ebcbff777f4bc301eb90eeb6473d4
Jason Sun <sunapi386@gmail.com>
2013-10-23 03:24:25 -0400

    5 welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Dota2matchmaker::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
+  resources :posts
+  root to: 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
